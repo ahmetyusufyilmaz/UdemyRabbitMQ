72d153c918b375d6800bdfbb723d36a61de6f195
Ahmet Yusuf Yılmaz <ahmetyilmaz@atlasyazilim.com.tr>
2022-09-05 16:38:14 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bzzotmns:u_1FPPhjTV1-emUaiViy1AmDr_gdSxoP@whale.rmq.cloudamqp.com/bzzotmns");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "hello world";
+
+            var messageBody= Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty,"hello-queue",null,messageBody);
+
+            Console.WriteLine("Mesaj gönderilmiştir");
+
+        }
+    }
+}
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
